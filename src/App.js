c3d9872fd850870621f359952350b283c4869cc8
john_wirihana_aupouri <johnaupouri963@gmail.com>
2020-02-04 22:03:30 +1300

    css complete

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const api = {
+  key: "ea23cc1013c5c99ee9fce1b248663c6d",
+  base: "https://api.openweathermap.org/data/2.5/"
 }
 
-export default App;
+function App() {
+
+  const dateBuilder = d => {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
+    let day = days[d.getDay()]
+    let date = d.getDate()
+    let month = months[d.getMonth()]
+    let year = d.getFullYear()
+
+    return `${day} ${date} ${month} ${year}`
+
+  }
+  return (
+    <div className="App">
+      <main>
+        <div className="search-box">
+          <input  
+            type="text"
+            className="search-bar"
+            placeholder="Search..."
+          />
+        </div>
+        <div>
+          <div className="location-box">
+            <div className="location">Wairoa, N.Z</div>
+            <div className="date">{dateBuilder(new Date())}</div>
+          </div>
+          <div className="weather-box">
+            <div className="temp">
+              13°C
+            </div>
+            <div className="date">
+              Cloudy
+            </div>
+          </div>
+          
+        </div>
+        
+      </main>   
+    </div>
+  )
+}
+
+export default App
